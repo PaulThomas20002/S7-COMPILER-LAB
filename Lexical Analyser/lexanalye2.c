399da69662638b12421f862fdf792ec5546146f8
Paul Thomas <66468931+PaulThomas20002@users.noreply.github.com>
2023-12-16 13:28:57 +0530

    Update lexanalye2.c

--- Lexical Analyser/lexanalye2.c
+++ Lexical Analyser/lexanalye2.c
@@ -2,68 +2,77 @@
 #include<ctype.h>
 #include<string.h>
 
-int main(){
+int main()
+{
 	FILE *input, *output;
 	int l=1;
 	int t=0;
 	int j=0;
-	int i,flag;
+	int i,flag=0;
 	char ch,str[20];
 	input = fopen("input.txt","r");
 	output = fopen("output.txt","w");
-	char keyword[30][30] = {"int","main","if","else","do","while"};
+	char keyword[30][30] = {"include","void","return","for",".h","int","main","if","else","do","while", "stdio"};
 	fprintf(output,"Line no. \t Token no. \t\t Token \t\t Lexeme\n\n");
 
-	while(!feof(input))	{
+	while(!feof(input)){
 		i=0;
 		flag=0;
 		ch=fgetc(input);
 
-		if( ch=='+' || ch== '-' || ch=='*' || ch=='/' )		{
-			fprintf(output,"%7d\t\t %7d\t\t Operator\t %7c\n",l,t,ch);
+		if( ch=='+' || ch== '-' || ch=='*' || ch=='/'|| ch == '=' ){
+			fprintf(output,"%7d\t\t %7d\t\t Operator\t\t %7c\n",l,t,ch);
 			t++;
 		}
-		else if( ch==';' || ch=='{' || ch=='}' || ch=='(' || ch==')' || ch=='?' || ch=='@' ||ch=='!' || ch=='%')		{
-			fprintf(output,"%7d\t\t %7d\t\t Special symbol\t %7c\n",l,t,ch);
+
+		else if( ch==';' || ch=='{' || ch=='}' || ch=='(' || ch==')' || ch=='?' || ch=='@' ||ch=='!' || ch=='%'){
+
+			fprintf(output,"%7d\t\t %7d\t\t Punctuations\t %7c\n",l,t,ch);
 			t++;
 		}
-		else if(isdigit(ch))		{
-			fprintf(output,"%7d\t\t %7d\t\t Digit\t\t %7c\n",l,t,ch);
+
+		else if(isdigit(ch)){
+			fprintf(output,"%7d\t\t %7d\t\t Literals\t\t %7c\n",l,t,ch);
 			t++;
 		}
-		else if(isalpha(ch))		{
+
+		else if(isalpha(ch)){
 			str[i]=ch;
 			i++;
 			ch=fgetc(input);
 
-			while(isalnum(ch) && ch!=' ')			{
+			while(isalnum(ch) && ch!=' '){
 				str[i]=ch;
 				i++;
 				ch=fgetc(input);
 			}
 			str[i]='\0';
-			for(j=0;j<=30;j++)			{
-				if(strcmp(str,keyword[j])==0)				{
+			
+			for(j=0;j<=12;j++){
+				if(strcmp(str, keyword[j])==0){
+					printf(keyword[j]);
+					printf(" Flag = %d\n", flag);
 					flag=1;
 					break;
 				}
 			}
-
-			if(flag==1)			{
-				fprintf(output,"%7d\t\t %7d\t\t Keyword\t %7s\n",l,t,str);
+			if(flag==1){
+				fprintf(output,"%7d\t\t %7d\t\t Keyword\t\t %7s\n",l,t,str);
 				t++;
 			}
-			else			{
-				fprintf(output,"%7d\t\t %7d\t\t Identifier\t %7s\n",l,t,str);
+			else{
+				fprintf(output,"%7d\t\t %7d\t\t Identifier\t\t %7s\n",l,t,str);
 				t++;
+				
 			}
+			flag = 0;
 		}
-		else if(ch=='\n')		{
+
+		else if(ch=='\n'){
 			l++;
 		}
 	}
 	fclose(input);
 	fclose(output);
 	return 0;
-
 	}
